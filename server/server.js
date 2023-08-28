2551aeff9e66a426a1a11ae8af2f552ffb769339
Shir Sharoni <shir.sharoni@wsc-sports.com>
2023-08-28 18:04:14 +0300

    Icons in popup fixed

--- server/server.js
+++ server/server.js
@@ -2,25 +2,19 @@ import express from "express";
 import axios from "axios";
 import cors from "cors";
 import bodyParser from "body-parser";
-import cron from "node-cron"; 
-
-
+import cron from "node-cron";
 const app = express();
 const port = 4000;
 const ComeetApiToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjSWdzMnJyWEFaeWZ5Q1huelFmd1lWeDduUUptanBTRFJKRjciLCJleHAiOiIzMDAxOTQ4ODc5In0.6WqCzMqn38QjpWdIF_d-W-WkQAa517qJva8gSRZZU7M";
-
 app.use(
   cors({
     origin: "http://localhost:3000",
   })
 );
-
 // example:  http://localhost:4000/WSCandidates/B8.85816
-
 app.get("/WSCandidates/:uid", async (req, res) => {
   const uid = req.params.uid;
-
   try {
     const response = await axios.get(
       `https://api.comeet.co/candidates/${uid}`,
@@ -30,24 +24,19 @@ app.get("/WSCandidates/:uid", async (req, res) => {
         },
       }
     );
-
     const candidateDataBeforeMerge = response.data;
-
     // merging and sorting the steps to an array called 'steps' and adding a 'status' filed that indicates if the step is 'completed', 'current' or 'future'.
     // deleting from the steps the first two: cv screen and phone call.
-
     function mergeSteps(candidateData) {
       const currentSteps = candidateData.current_steps || [];
       const completedSteps = candidateData.completed_steps || [];
       const futureSteps = candidateData.future_steps || [];
-
       // Sort the completed steps by time_completed in ascending order
       const sortedCompletedSteps = completedSteps.slice().sort((a, b) => {
         const timeA = new Date(a.time_completed);
         const timeB = new Date(b.time_completed);
         return timeA - timeB; // ascending order
       });
-
       const allSteps = [
         ...sortedCompletedSteps,
         ...currentSteps,
@@ -61,12 +50,10 @@ app.get("/WSCandidates/:uid", async (req, res) => {
           step.name !== "CV Screen / Recruiter" &&
           step.name !== "Phone screen / Recruiter"
       );
-
       const latestCompletedStep = getLatestCompletedStep(
         currentSteps,
         sortedCompletedSteps
       );
-
       candidateData.steps = stepsToKeep.map((step) => {
         if (currentSteps.length === 0) {
           step.status = step === latestCompletedStep ? "current" : "future";
@@ -98,21 +85,17 @@ app.get("/WSCandidates/:uid", async (req, res) => {
         }
         return step;
       });
-
       delete candidateData.current_steps;
       delete candidateData.completed_steps;
       delete candidateData.future_steps;
-
       return candidateData;
     }
-
     function currentStepHasNonNullScheduledTime(currentSteps, stepToCheck) {
       const currentStep = currentSteps.find(
         (step) => step.position_step_uid === stepToCheck.position_step_uid
       );
       return currentStep && currentStep.time_scheduled !== null;
     }
-
     function getLatestCompletedStep(currentSteps, completedSteps) {
       const latestCompletedStep = currentSteps.find(
         (currentStep) => currentStep.time_scheduled === null
@@ -127,9 +110,7 @@ app.get("/WSCandidates/:uid", async (req, res) => {
       }
       return null;
     }
-
     // cheacking if the candidate is Rejected and hide it if we need.
-
     function updateStatus(candidateData) {
       if (
         candidateData.status === "Rejected" &&
@@ -139,13 +120,35 @@ app.get("/WSCandidates/:uid", async (req, res) => {
       }
       return candidateData;
     }
-
+    async function getPositionName(positionUid) {
+      try {
+        const response = await axios.get(
+          `https://api.comeet.co/positions/${positionUid}`,
+          {
+            headers: {
+              Authorization: `Bearer ${ComeetApiToken}`,
+            },
+          }
+        );
+        return response.data.name; // Assuming the position name is present in the response
+      } catch (error) {
+        console.error(
+          `Error fetching position name for UID ${positionUid}:`,
+          error
+        );
+        return null;
+      }
+    }
     const candidateDataAfterUpdate = updateStatus(candidateDataBeforeMerge);
     const candidateData = mergeSteps(candidateDataAfterUpdate);
-
+    const positionUid = candidateData.position_uid;
+    const positionName = await getPositionName(positionUid);
+    candidateData.steps = candidateData.steps.map((step) => ({
+      ...step,
+      position_name: positionName,
+    }));
     // Send the candidate information as a JSON response
     res.json(candidateData);
-
     // const candidateData = response.data;
     // res.json(candidateData);
   } catch (error) {
@@ -153,7 +156,6 @@ app.get("/WSCandidates/:uid", async (req, res) => {
     res.status(404).json({ error: "Candidate not found" });
   }
 });
-
 // Schedule a task to run every 5 minutes
 cron.schedule("*/5 * * * *", async () => {
   try {
@@ -163,48 +165,49 @@ cron.schedule("*/5 * * * *", async () => {
         Authorization: `Bearer ${ComeetApiToken}`,
       },
     });
-
     const candidates = response.data.candidates;
-
     for (const candidate of candidates) {
       if (candidate.deleted === false) {
-      
         //console.log(` ${candidate.uid} in the loop`);
-
-        const customFieldsIsEmpty = Object.keys(candidate.custom_fields).length === 0;
-
+        const customFieldsIsEmpty =
+          Object.keys(candidate.custom_fields).length === 0;
         if (customFieldsIsEmpty) {
           // 'custom_fields' === null (new candidate with no url)
           console.log(
             `Candidate with UID ${candidate.uid} has a null portalUrl. Updating portalUrl...`
           );
           try {
-              await axios.patch(`https://api.comeet.co/candidates/${candidate.uid}`, JSON.stringify({
+            await axios.patch(
+              `https://api.comeet.co/candidates/${candidate.uid}`,
+              JSON.stringify({
                 updated_by: {
-                  type: 'app',
-                  name: 'WSC-combine'
+                  type: "app",
+                  name: "WSC-combine",
                 },
-                "custom_fields": {
-                    "portalUrl": `http://localhost:4000/WSCandidates/${candidate.uid}`
-                  }
-              }), {
+                custom_fields: {
+                  portalUrl: `http://localhost:4000/WSCandidates/${candidate.uid}`,
+                },
+              }),
+              {
                 headers: {
-                  'Content-Type': 'application/json', // Set content type to JSON
-                  Authorization: `Bearer ${ComeetApiToken}`
-                }
-              });
-              console.log(`portalURL updated for candidate ${candidate.uid}`);
-            } catch (error) {
-              console.error(`Error updating portalURL for candidate ${candidate.uid}: ${error.message}`);
-            }
+                  "Content-Type": "application/json", // Set content type to JSON
+                  Authorization: `Bearer ${ComeetApiToken}`,
+                },
+              }
+            );
+            console.log(`portalURL updated for candidate ${candidate.uid}`);
+          } catch (error) {
+            console.error(
+              `Error updating portalURL for candidate ${candidate.uid}: ${error.message}`
+            );
           }
         }
       }
-      } catch (error) {
-        console.error("Error fetching candidate list:", error);
-      }
-      });
-
+    }
+  } catch (error) {
+    console.error("Error fetching candidate list:", error);
+  }
+});
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
--- sportsApplication/src/components/navbar.js
+++ sportsApplication/src/components/navbar.js
@@ -1,40 +1,41 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function HomeNavbar() {
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="https://wsc-sports.com/">WSC Sports</Navbar.Brand>
+          <Navbar.Brand href="https://wsc-sports.com/" target="_blank">
+            WSC Sports
+          </Navbar.Brand>
           <Nav className="justify-content-end">
             <Nav.Link href="https://www.linkedin.com/company/wsc-sports-technologies/">
-                <span style={{ color: "white" }}>
-                    <i aria-hidden="true" class="fab fa-linkedin"></i>
-                </span>
-                </Nav.Link>
+              <span style={{ color: "white" }}>
+                <i aria-hidden="true" class="fab fa-linkedin"></i>
+              </span>
+            </Nav.Link>
             <Nav.Link href="https://twitter.com/wsc_sports">
-            <span class="elementor-icon-list-icon" style={{ color: "white" }}>
-                    <i aria-hidden="true" class="fab fa-twitter"></i>
-                </span>
-                </Nav.Link>
+              <span class="elementor-icon-list-icon" style={{ color: "white" }}>
+                <i aria-hidden="true" class="fab fa-twitter"></i>
+              </span>
+            </Nav.Link>
             <Nav.Link href="https://www.instagram.com/wsc_sports/">
-            <span class="elementor-icon-list-icon" style={{ color: "white" }}>
-                    <i aria-hidden="true" class="fab fa-instagram"></i>
-                </span>
+              <span class="elementor-icon-list-icon" style={{ color: "white" }}>
+                <i aria-hidden="true" class="fab fa-instagram"></i>
+              </span>
             </Nav.Link>
             <Nav.Link href="https://www.facebook.com/WSC.SportsTechnologies/">
-            <span class="elementor-icon-list-icon" style={{ color: "white" }}>
-                    <i aria-hidden="true" class="fab fa-facebook"></i>
-                </span>
+              <span class="elementor-icon-list-icon" style={{ color: "white" }}>
+                <i aria-hidden="true" class="fab fa-facebook"></i>
+              </span>
             </Nav.Link>
             <Nav.Link href="https://sports-content-kings.simplecast.com/">
-            <span class="elementor-icon-list-icon" style={{ color: "white" }}>
-                    <i aria-hidden="true" class="fas fa-microphone-alt"></i>
-                </span>
+              <span class="elementor-icon-list-icon" style={{ color: "white" }}>
+                <i aria-hidden="true" class="fas fa-microphone-alt"></i>
+              </span>
             </Nav.Link>
-            
           </Nav>
         </Container>
       </Navbar>
@@ -43,4 +44,3 @@ function HomeNavbar() {
 }
 
 export default HomeNavbar;
-
