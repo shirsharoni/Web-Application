4d93dfee0e7295ee8b6e1555d109ad2a8d2692f3
Rony Avivi <rony.avivi@wsc-sports.com>
2023-08-28 10:08:26 +0300

    New navbar

--- sportsApplication/src/App.js
+++ sportsApplication/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-// import { useEffect, useState } from 'react';
-// import axios from 'axios';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Home from './components/Home';
 import SoccerPage from './components/SoccerPage';
 import BasketballPage from './components/BasketballPage';
 import './components/ApiData.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import './AppHamburger.css';
-// import './App.css';
+
 
 import {
   BrowserRouter as Router,
