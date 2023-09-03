6308936ab403943376ec89ea3422274203462b38
Shir Sharoni <shir.sharoni@wsc-sports.com>
2023-09-03 16:52:14 +0300

    Fixed Soccer page with interview information

--- sportsApplication/src/components/SoccerPage/PlayerPopup.js
+++ sportsApplication/src/components/SoccerPage/PlayerPopup.js
@@ -1,26 +1,21 @@
 import React from "react";
-import { Modal } from "react-bootstrap";
-import { Button } from "react-bootstrap";
-import DarkMode from "../toggle/DarkMode";
+import Button from "react-bootstrap/Button";
 
-export function PlayerPopup({ togglePopUp, player }) {
+export function PlayerPopup({ player, togglePopUp }) {
   return (
-    <Modal show={true} onHide={togglePopUp} centered>
-      <Modal.Header closeButton>
-        <Modal.Title>Interview Information</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h2>For which position?</h2>
-        {/* <p>{player.position_name}</p> */}
-        <h2>With whom?</h2>
-        <ul>
-          {player.assignees.map((assignee, index) => (
-            <li key={index}>
-              {assignee.first_name} {assignee.last_name} <br /> Interviewer
-              email: ({assignee.email})
-            </li>
-          ))}
-        </ul>
+    <div className="PopUp">
+      <Button
+        className="close-button btn btn-secondary"
+        type="button"
+        onClick={togglePopUp}
+        border-radius="45%"
+      >
+        <div>x</div>
+      </Button>
+      <h1>Interview information</h1>
+      <div>
+        <h2>What to expect?</h2>
+        <p>{player.name}</p>
         <h2>When is the interview?</h2>
         {player.time_scheduled ? (
           <div>
@@ -30,45 +25,16 @@ export function PlayerPopup({ togglePopUp, player }) {
         ) : (
           <p>Not scheduled yet</p>
         )}
-        <h2>What to expect?</h2>
-        <p>{player.name}</p>
-        <a
-          href="https://goo.gl/maps/wGh5ppSvMwv4Vudo8"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="btn btn-link"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            position: "absolute",
-            bottom: "20px",
-            right: "20px",
-          }}
-        >
-          <span style={{ padding: ".25rem .5rem", fontSize: "1.75rem" }}>
-            <i className="fa-solid fa-map-pin"></i>
-          </span>
-        </a>
-        <a
-          href="https://www.waze.com/live-map/directions/il/tel-aviv-district/%D7%A8%D7%92/wsc-sports?to=place.ChIJt0QWvcBLHRURRiGcBtZacYs&from=ll.32.0863353%2C34.8049162&utm_medium=lm_share_directions&utm_campaign=default&utm_source=waze_website"
-          target="_blank"
-          className="btn btn-link"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            position: "absolute",
-            bottom: "20px",
-            right: "80px",
-          }}
-        >
-          <span style={{ padding: ".25rem .5rem", fontSize: "1.75rem" }}>
-            <i className="fab fa-waze"></i>
-          </span>
-        </a>
-      </Modal.Body>
-      <DarkMode />
-    </Modal>
+      </div>
+      <h2>Who are the interviewers?</h2>
+      <ul>
+        {player.assignees.map((assignee, index) => (
+          <li key={index}>
+            {assignee.first_name} {assignee.last_name} <br /> Interviewer email:
+            ({assignee.email})
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
--- sportsApplication/src/components/SoccerPage/SoccerPage.jsx
+++ sportsApplication/src/components/SoccerPage/SoccerPage.jsx
@@ -7,29 +7,18 @@ import "../../AppHamburger.css";
 import "../../PopUp.css";
 import "./SoccerPage.css";
 import HomeNavbar from "../navbar";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Carousel from 'react-bootstrap/Carousel';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import DarkMode from "../toggle/DarkMode.js";
-
-// Import the helper functions from helpers.js
-import {
-  getCurrentStepName,
-  getCurrentStepAssignees,
-  getCurrentStepAssigneesAndTimeScheduled,
-  formatTime,
-} from "./helpers";
 
 function SoccerPage() {
   const { id } = useParams();
   const { data, error, isLoading } = useAppData(id);
-  const [isPopUpVisible, setPopUpVisible] = useState(false);
-  const [popupId, setPopUpId] = useState(false);
-  const [backgroundColor, setBackgroundColor] = useState("beige"); // Add background color state
+  const [selectedPlayerIndex, setSelectedPlayerIndex] = useState(null);
+  const [backgroundColor, setBackgroundColor] = useState("beige");
 
   const handleToggleTheme = (isDarkMode) => {
-    // Update the background color based on the theme
     setBackgroundColor(isDarkMode ? "lightgreen" : "beige");
   };
 
@@ -39,60 +28,18 @@ function SoccerPage() {
   if (error) {
     return <div>Error: {error}</div>;
   }
-  const numOfPlayers = data ? data.steps.length : 0;
 
-
-  const togglePopUp = (i) => {
-    setPopUpVisible(!isPopUpVisible);
-    setPopUpId(i);
-  };
-
-  const renderPlayers = (num) => {
-    const players = [];
-
-    for (let i = 0; i < num; i++) {
-      const player = data.steps[i];
-      players.push(
-        <div className={`player-container`} key={i}>
-          <img
-            className={`overlay-image_${i + 1}`}
-            src={`/soccer-players/Soccer_player${i + 1}.png`}
-            alt={`soccer-player${i + 1}`}
-            width="100%"
-            onClick={() => togglePopUp(i)}
-          />
-          {isPopUpVisible && i === popupId && (
-            <PlayerPopup togglePopUp={() => togglePopUp(i)} player={player} />
-          )}
-        </div>
-      );
-    }
-    return players;
-  };
-
-
-  function generateCirclesWithSteps(steps) {
-    return steps.map((step, index) => (
-      <Col key={index}>
-        <div
-          className={`circular`}
-          onClick={() => handleCircleClick(index)}
-        >
-          <span className="ball_num">{index + 1}</span>
-        </div>
-      </Col>
-    ));
+  // Find the index of the step with the current status and set it as the default selected step.
+  const currentIndex = data.steps.findIndex(
+    (step) => step.status === "current"
+  );
+  if (selectedPlayerIndex === null) {
+    setSelectedPlayerIndex(currentIndex);
   }
-  
 
   function handleCircleClick(index) {
-    // You can access the corresponding step using the 'index' parameter
-    const selectedStep = data.steps[index];
-    // Do something with the selected step, e.g., display its details
-    console.log(`Clicked on circle ${index + 1}, step details:`, selectedStep);
+    setSelectedPlayerIndex(index); // Update selected player index when a circle is clicked
   }
-  
-
 
   return (
     <>
@@ -101,41 +48,69 @@ function SoccerPage() {
         <DarkMode onToggleTheme={handleToggleTheme} />
         <div className="container container-soccer d-flex flex-column vh-100">
           <div id="h1">{data.position_name}</div>
+          <div id="h2">
+            {data.first_name} {data.last_name}
+          </div>
 
-          <div id="h2">{data.first_name} {" "} {data.last_name}</div>
-          
           <Container fluid="true" className="inner-container">
             <Row>
-              {generateCirclesWithSteps(data.steps)}
+              {data.steps.map((step, index) => (
+                <Col key={index}>
+                  <div
+                    className={`circular`}
+                    onClick={() => handleCircleClick(index)}
+                  >
+                    <span className="ball_num">{index + 1}</span>
+                  </div>
+                </Col>
+              ))}
             </Row>
           </Container>
-        
-          {/* <div class="inner-container"> */}
-  
-            {/* <Container className="soccer-comtainer"> */}
-              {/* <div id="soccer-page"> */}
-                {/* {renderPlayers(numOfPlayers)} */}
-              {/* </div> */}
-            {/* </Container> */}
-          {/* </div> */}
 
-          {/* </Container> */}
-
-
-          <div>
-            <div id="p">
-              {" "}
-              <span className="interview_header">Current Step:</span>{" "}
-              {getCurrentStepName(data.steps)} <br />
-              <span className="interview_header">Interviewers:</span>{" "}
-              {getCurrentStepAssignees(data.steps)} <br />
-              <span className="interview_header">Time Scheduled:</span>{" "}
-              {formatTime(
-                getCurrentStepAssigneesAndTimeScheduled(data.steps)
-                  .timeScheduled
-              )}
-            </div>
-          </div>
+          {/* Player details container */}
+          {selectedPlayerIndex !== null && (
+            <Container fluid="true" className="container-soccer">
+              <Row>
+                <Col>
+                  <h1>Interview information</h1>
+                  <div>
+                    <h2>What to expect?</h2>
+                    <p>{data.steps[selectedPlayerIndex].name}</p>
+                    <h2>When is the interview?</h2>
+                    {data.steps[selectedPlayerIndex].time_scheduled ? (
+                      <div>
+                        <p>
+                          Date:{" "}
+                          {new Date(
+                            data.steps[selectedPlayerIndex].time_scheduled
+                          ).toLocaleDateString()}
+                        </p>
+                        <p>
+                          Time:{" "}
+                          {new Date(
+                            data.steps[selectedPlayerIndex].time_scheduled
+                          ).toLocaleTimeString()}
+                        </p>
+                      </div>
+                    ) : (
+                      <p>Not scheduled yet</p>
+                    )}
+                  </div>
+                  <h2>Who are the interviewers?</h2>
+                  <ul>
+                    {data.steps[selectedPlayerIndex].assignees.map(
+                      (assignee, index) => (
+                        <li key={index}>
+                          {assignee.first_name} {assignee.last_name} <br />{" "}
+                          Interviewer email: ({assignee.email})
+                        </li>
+                      )
+                    )}
+                  </ul>
+                </Col>
+              </Row>
+            </Container>
+          )}
         </div>
       </div>
     </>
